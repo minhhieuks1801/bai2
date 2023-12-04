f4dcfa802c383d6c811381c6f9caa4b1689027f3
minhhieuks1801 <minhhieuks1801@gmail.com>
2023-12-04 17:39:02 +0700

    hieu

---
+++ untitled1/lib/xemThoiTiet.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class xemThoiTiet extends StatefulWidget{
+  const xemThoiTiet({super.key});
+  @override
+  _xemThoiTiet1 createState() => _xemThoiTiet1();
+}
+
+
+class _xemThoiTiet1 extends State<xemThoiTiet> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Xem thời tiết')),
+      body: Center(
+
+      ),
+    );
+  }
+
+}
